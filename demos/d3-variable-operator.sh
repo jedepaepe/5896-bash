51a4d710278fd28014b35a59e76600af5113ffd2
jedepaepe <jedepaepe@epfc.eu>
2025-11-21 15:34:49 +0100

    variables & rename

---
+++ demos/d3-variable-operator.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+a=2
+b=3
+
+echo $a + $b = $((a+b))
+echo $a - $b = $((a-b))
+echo $a \* $b = $((a*b))
+echo $a / $b = $((a/b))
+echo $a % $b = $((a%b))    - reste de la division
+echo $a exposant $b = $((a**b))
