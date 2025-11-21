b09b6b63e160e968c9c96e77ddb2e28730fa1e2d
jedepaepe <jedepaepe@epfc.eu>
2025-11-21 15:01:27 +0100

    printf

---
+++ demos/d2-printf-format.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+printf "%-10s | %-10s\n" Pays Capitale
+printf "%-10s | %-10s\n" Belgique Bruxelles
+printf "%-10s | %-10s\n" France Paris
+printf "%-10s | %-10s\n" Allemagne Berlin
+printf "\n"
+printf "nombre : %d\n" 36
+printf "nombre : %1d\n" 36
+printf "nombre : %3d\n" 36
+printf "nombre : %4d\n" 36
+printf "nombre : %5d\n" 36
+printf "\n"
+printf "nombre : %0d\n" 36
+printf "nombre : %01d\n" 36
+printf "nombre : %03d\n" 36
+printf "nombre : %04d\n" 36
+printf "nombre : %05d\n" 36
---
+++ demos/d2-printf.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+printf "##################\n"
+printf "#                #\n"
+printf "#                #\n"
+printf "#   BONJOUR      #\n"
+printf "#                #\n"
+printf "#                #\n"
+printf "##################\n"
