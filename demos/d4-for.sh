8ae6dbfa15f46118c531ab822dc7c1d82cecccc6
jedepaepe <jedepaepe@epfc.eu>
2025-11-21 15:55:58 +0100

    for

---
+++ demos/d4-for.sh
@@ -0,0 +1,17 @@
+echo example 1, serie simple
+for v1 in le la 33 les; do
+  echo "  $v1"
+done
+echo
+
+echo example 2, série d'entiers
+for v1 in {5..9}; do
+  echo "  $v1"
+done
+echo
+
+echo example 3, résultat d'une commande, ls ici
+for v1 in $(ls /); do
+  echo "  $v1"
+done
+echo
