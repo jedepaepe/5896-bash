b16a0d3c5584971dc04c9a988dd67d935bcd5cea
jedepaepe <jedepaepe@epfc.eu>
2025-11-21 16:17:27 +0100

    read

---
+++ demos/d5-read.sh
@@ -0,0 +1,13 @@
+printf "Nom : "
+read family_name
+
+printf "Prénom : "
+read first_name
+
+printf "Téléphone : "
+read phone
+
+printf "Email : "
+read email
+
+printf "%s %s %s %s\n" $family_name $first_name $phone $email
