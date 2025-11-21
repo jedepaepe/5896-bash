d1862e4f7a0c4c93314ebaacd9238d8af36730d0
jedepaepe <jedepaepe@epfc.eu>
2025-11-21 13:03:42 +0100

    exercice 1

---
+++ exercies/e1-echo.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+echo "Bonjour à tous!"
+echo "Ceci est mon premier exercice."
+echo "bash Bourne Again SHell."
+echo "Bourne shell est le shell par défaut de la version 7 de Unix, 1977."
+echo "bash est une réécriture du bourne shell avec améliorations et sous license GNU."
