07d0a3d1aa62fd6aa7096004ddae84e5c6c901b3
jedepaepe <jedepaepe@epfc.eu>
2025-11-21 13:05:06 +0100

    ex 1a

--- exercies/e1a-echo.sh
+++ exercies/e1a-echo.sh
@@ -4,3 +4,7 @@ echo "Ceci est mon premier exercice."
 echo "bash Bourne Again SHell."
 echo "Bourne shell est le shell par défaut de la version 7 de Unix, 1977."
 echo "bash est une réécriture du bourne shell avec améliorations et sous license GNU."
+echo 
+echo
+echo "    -       "
+echo
