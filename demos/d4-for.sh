06bfa3ad34895fda554a45624801724a2cb56d9d
jedepaepe <jedepaepe@epfc.eu>
2025-11-29 15:25:16 +0100

    amélioration

--- demos/d4-for.sh
+++ demos/d4-for.sh
@@ -1,17 +1,17 @@
 #!/usr/bin/env bash
-echo example 1, serie simple
-for v1 in le la 33 les; do
+echo "example 1, serie simple"
+for v1 in "le" "la" "33" "les"; do
   echo "  $v1"
 done
 echo
 
-echo example 2, série d'entiers
+echo "example 2, série d'entiers"
 for v1 in {5..9}; do
   echo "  $v1"
 done
 echo
 
-echo example 3, résultat d'une commande, ls ici
+echo "example 3, résultat d'une commande, ls ici"
 for v1 in $(ls /); do
   echo "  $v1"
 done
--- demos/d7-quality-set-o-pipefail.sh
+++ demos/d7-quality-set-o-pipefail.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 set -o pipefail
 
+# shellcheck disable=SC2216
 echo "test 1" | false | echo "fin test 1"
 echo "Résultat de la ligne précédente $?"
 echo
 
+# shellcheck disable=SC2012,SC2216
 echo "test 2" | ls fichier_qui_n_existe_pas | echo "fin test 2"
 echo "Résultat de la ligne précédente $?"
