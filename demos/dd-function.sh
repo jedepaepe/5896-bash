30b63567179e0a20f6e5156c5956588e6bcfab3e
Jean-Michel Depaepe <jedepaepe@epfc.eu>
2025-12-12 20:39:09 +0100

    function

---
+++ demos/dd-function.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+set -eo pipefail
+
+print_message() {
+  echo 'Amusons-nous avec bash'
+  echo 'Un langage de script très utilisé sous linux'
+  echo 'Pour administrer les serveurs'
+}
+
+print_message
