0e336cb8f1c0ca6ce5e0a39404380bcaccb46ed0
Jean-Michel Depaepe <jedepaepe@epfc.eu>
2025-12-12 08:56:39 +0100

    better " => '

--- demos/d1-echo.sh
+++ demos/d1-echo.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-echo "Amusons-nous avec bash"
-echo "Un langage de script très utilisé sous linux"
-echo "Pour administrer les serveurs"
+echo 'Amusons-nous avec bash'
+echo 'Un langage de script très utilisé sous linux'
+echo 'Pour administrer les serveurs'
