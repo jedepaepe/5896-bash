2ec1d0a047d4403e1ccc6377cdd09f3e39f4f47a
Jean-Michel Depaepe <jedepaepe@epfc.eu>
2025-12-12 09:11:43 +0100

    better

--- demos/d3-variable.sh
+++ demos/d3-variable.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
-var1="bonjour"
-var2="le"
-var3="monde"
+var1='bonjour'
+var2='le'
+var3='monde'
 
 echo $var1 $var2 $var3
 
 i=21
-printf "Vingt et un : %04d\n\n\n" $i
+printf 'Vingt et un : %04d\n\n\n' $i
