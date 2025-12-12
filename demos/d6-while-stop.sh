5ed55a57d5c2f0340c12a3bb344895c88171903b
Jean-Michel Depaepe <jedepaepe@epfc.eu>
2025-12-12 15:39:14 +0100

    better

--- demos/d6-while-stop.sh
+++ demos/d6-while-stop.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
-run="R"
-while [[ "${run}" != "S" ]]; do
-  read -r -p "tapez S pour arrêter le script : " run
+run='R'
+while [[ "$run" != 'S' ]]; do
+  read -r -p 'tapez S pour arrêter le script : ' run
   echo "---> $run"
 done
-echo "ciao"
+echo ciao
