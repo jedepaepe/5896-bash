b3058cbe18dcb9629c3cceb74b5d3fde8e33dce6
Jean-Michel Depaepe <jedepaepe@epfc.eu>
2025-12-13 18:00:54 +0100

    email

---
+++ demos/dd-function-email.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+set -o pipefail
+
+EMAIL_REGEX='.{2}.*@.*\..*'
+
+if [[ "$1" =~ $EMAIL_REGEX ]]; then
+  echo "$1 est un email valide"
+  exit 0
+fi
+
+echo "$1 est un email invalide"
+exit 2
