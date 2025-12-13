210ab967263d1a1f40f30d8ad8f559559b91d210
Jean-Michel Depaepe <jedepaepe@epfc.eu>
2025-12-13 19:14:09 +0100

    more

---
+++ more-ex/email/email-3.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+set -o pipefail
+
+EMAIL_REGEX="^.*.@..*$"
+
+if [[ "$1" =~ $EMAIL_REGEX ]]; then
+  echo "$1 est un email valide"
+  exit 0
+fi
+echo "$1 n'est pas un email valide"
+exit 2
